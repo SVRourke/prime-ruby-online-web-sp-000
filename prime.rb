e2be3301fc7cef4cf06252e5ec666f3e412a8f60
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 04:33:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,34 @@
 # Add  code here!
+def prime?(n)
+  case
+    when n <= 1
+      false
+    when n == 2 || n == 3
+      true
+    when n.even?
+      puts"even"
+      false
+    else
+      factors = Array(2..n-1)
+      factors = factors.map {|factor| n % factor == 0 ? true : false }
+      ? factors.include?(true)
+    end
+  end
+  
+#   if n <= 1
+#     false
+#   end
+  
+#   if n == 2 || n == 3
+#     true
+#   end
+  
+#   if n.even?
+  
+#   factors = Array(2..n-1)
+#   factors = factors.map {|factor| n % factor == 0 ? true : false }
+#   factors.include?(true)
+#   # true
+# end
+
+puts prime?(11)
